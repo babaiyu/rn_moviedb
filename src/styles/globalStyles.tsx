2d7c5ba4e860a7e369cecb7d22585896a7f245a3
babaiyu <bayuputra9a@gmail.com>
2021-07-08 21:59:48 +0700

    Add globalStyles

---
+++ src/styles/globalStyles.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default globalStyles;
