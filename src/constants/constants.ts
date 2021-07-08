c7b90280e0d1ef70d76d403c9305c8fd474a23fe
babaiyu <bayuputra9a@gmail.com>
2021-07-08 21:39:30 +0700

    New contants

---
+++ src/constants/constants.ts
@@ -0,0 +1,16 @@
+export const fonts = {
+  bold: 'Poppins-Bold',
+  medium: 'Poppins-Medium',
+  regular: 'Poppins-Regular',
+};
+
+export const myColors = {
+  dark: '#000a12',
+  semiDark: '#263238',
+  light: '#f0f0f0',
+  blue: '#002f6c',
+  semiBlue: '#01579b',
+  danger: '#b71c1c',
+  warning: '#ffb300',
+  success: '#388e3c',
+};
