e08f78ec47f43e1fc7eed94a48c0279ffd533e49
babaiyu <bayuputra9a@gmail.com>
2021-07-09 06:48:52 +0700

    Update homescreen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import {Text} from 'react-native-paper';
-import {View} from 'react-native';
+import {FlatList, SafeAreaView, RefreshControl} from 'react-native';
+import {useStateContext} from 'states/store';
+import useHome from 'hooks/useHome';
+import Loading from 'components/Loading';
+import MyCard from 'components/MyCard';
 
 export default function HomeScreen() {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  );
+  const {state} = useStateContext();
+  const {home} = state;
+
+  const {loading, error, getMovies} = useHome();
+
+  const renderItem = ({item}: any) => <MyCard {...item} />;
+  const keyExtractor = (i: any, index: number) => index.toString();
+
+  if (loading) return <Loading />;
+
+  if (error) return <Text>Error</Text>;
+  else
+    return (
+      <SafeAreaView>
+        <FlatList
+          data={home}
+          renderItem={renderItem}
+          keyExtractor={keyExtractor}
+          refreshControl={
+            <RefreshControl onRefresh={getMovies} refreshing={false} />
+          }
+        />
+      </SafeAreaView>
+    );
 }
