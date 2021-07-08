11527705e22518377579597fcc51c500aba42094
babaiyu <bayuputra9a@gmail.com>
2021-07-08 15:55:59 +0700

    Installing react-navigation

---
+++ src/config/routes.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+// Import Screens
+import HomeScreen from '../screens/Home';
+import AboutScreen from '../screens/About';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function BottomTab() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="HomeScreen" component={HomeScreen} />
+      <Tab.Screen name="AboutScreen" component={AboutScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={() => ({...TransitionPresets.SlideFromRightIOS})}>
+        <>
+          <Stack.Screen
+            name="BottomTab"
+            component={BottomTab}
+            options={{headerShown: false}}
+          />
+        </>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
