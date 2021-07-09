65af4e198a22e92de110731bfe4431bb0b8a5576
babaiyu <bayuputra9a@gmail.com>
2021-07-09 16:28:24 +0700

    Add reducer search

--- src/states/types.ts
+++ src/states/types.ts
@@ -3,15 +3,18 @@ import {Dispatch} from 'react';
 export enum Type {
   HOME = 'HOME',
   DETAIL = 'DETAIL',
+  SEARCH = 'SEARCH',
 }
 
 export type Action =
   | {type: Type.HOME; data: any[]}
-  | {type: Type.DETAIL; data: any[]};
+  | {type: Type.DETAIL; data: any[]}
+  | {type: Type.SEARCH; data: any[]};
 
 export interface State {
   home: any[];
   detail: any;
+  search: any[];
 }
 
 export interface Store {
