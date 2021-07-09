dbbdd812ae24b60e9f2c63ab50754f9e1d13c0aa
babaiyu <bayuputra9a@gmail.com>
2021-07-09 08:46:01 +0700

    Update myCard

--- src/components/MyCard.tsx
+++ src/components/MyCard.tsx
@@ -1,15 +1,23 @@
-import {Card, Title, Paragraph, Button} from 'react-native-paper';
+import React from 'react';
+import {Card, Paragraph, Button} from 'react-native-paper';
+import {StyleSheet} from 'react-native';
+import dayjs from 'dayjs';
+import {maxParagraph} from 'helpers';
 import {IMAGE_URL} from 'services/api';
 
 export default function MyCard(props: any) {
+  const paragraph = maxParagraph(props?.overview);
+  const subtitle = `${dayjs(props?.release_date).format('DD MMM, YYYY')} | ${
+    props?.vote_average
+  }`;
+
   return (
-    <Card>
-      <Card.Title title={props?.title} subtitle={props?.vote_average} />
-      <Card.Content>
-        <Title>{props?.title}</Title>
-        <Paragraph>{props?.overview}</Paragraph>
-      </Card.Content>
+    <Card style={styles.card}>
+      <Card.Title title={props?.title} subtitle={subtitle} />
       <Card.Cover source={{uri: IMAGE_URL + props?.poster_path}} />
+      <Card.Content>
+        <Paragraph>{paragraph}</Paragraph>
+      </Card.Content>
       <Card.Actions>
         <Button>Cancel</Button>
         <Button>Ok</Button>
@@ -17,3 +25,9 @@ export default function MyCard(props: any) {
     </Card>
   );
 }
+
+const styles = StyleSheet.create({
+  card: {
+    marginVertical: 5,
+  },
+});
