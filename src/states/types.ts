1fa3377393b85270ebf3b549542d1a3de21ad139
babaiyu <bayuputra9a@gmail.com>
2021-07-09 15:16:11 +0700

    Update states (detail)

--- src/states/types.ts
+++ src/states/types.ts
@@ -2,12 +2,16 @@ import {Dispatch} from 'react';
 
 export enum Type {
   HOME = 'HOME',
+  DETAIL = 'DETAIL',
 }
 
-export type Action = {type: Type.HOME; data: any[]};
+export type Action =
+  | {type: Type.HOME; data: any[]}
+  | {type: Type.DETAIL; data: any[]};
 
 export interface State {
   home: any[];
+  detail: any;
 }
 
 export interface Store {
